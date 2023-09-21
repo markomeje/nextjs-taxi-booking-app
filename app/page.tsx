acfc6eb2e4c634ce69090ba8a3877d9e83de91f6
markomeje <markomejeonline@gmail.com>
2023-09-21 22:34:53 +0100

    Layout and Autocomplete Address task

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+import Booking from '@/components/Booking/Booking'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <div>
-     
-    <h2>
-      juliet
-    </h2>
-   </div>
+    <div>
+      <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className=''>
+          <Booking />
+        </div>
+        <div className='col-span-2 bg-cyan-500'>
+          Map
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ components/Booking/AutocompleteAddress.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function AutocompleteAddress() {
+  return (
+    <div className='mt-5'>
+      <div>
+        <label className='text-gray-400'>Where From?</label>
+        <input type='text' className='rounded-md mt-2 bg-white p-1 border-[1px] focus:border-yellow-300 ouline-none w-full' />
+      </div>
+      <div className='mt-4'>
+        <label className='text-gray-400'>Where From?</label>
+        <input type='text' className='rounded-md mt-2 bg-white p-1 border-[1px] focus:border-yellow-300 ouline-none w-full' />
+      </div>
+    </div>
+  )
+}
